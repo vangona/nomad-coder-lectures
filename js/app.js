0138f9f1bd71fdee62a3aa9ddf84281d28a5eab6
vangona <rlarhksrud12@naver.com>
2021-05-23 00:16:00 +0900

    2021.05.23 ball update

--- js/app.js
+++ js/app.js
@@ -2,12 +2,18 @@ const canvas = document.getElementById("jsCanvas")
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
+// const radius = document.getElementById("jsRadius");
 const mode = document.getElementById("jsMode");
 const clear = document.getElementById("jsClear");
 const saveBtn = document.getElementById("jsSave")
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
+
+// let radiusSize = radius.value;
+let balls=[];
+let ballNumber = 0;
+let color = "rgb(0, 0, 0)";
 
 canvas.width = CANVAS_SIZE
 canvas.height = CANVAS_SIZE
@@ -20,6 +26,8 @@ ctx.lineWidth = 2.5;
 
 let painting = false;
 let filling = false;
+let balling = false;
+let rainbow = false;
 
 function stopPainting(){
     painting = false;
@@ -29,9 +37,78 @@ function startPainting(){
     painting = true;
 }
 
+class Ball {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.c = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
+        this.size = 10 + Math.random()*30;
+        this.directionX = Math.random()*10 - 5;
+        this.directionY = Math.random()*10 - 5;
+    }
+    update(){
+        this.x += this.directionX;
+        this.y += this.directionY;
+        if(this.x + this.size >= canvas.height || this.x - this.size <= 0){
+            this.directionX *= -1
+        }
+        if(this.y + this.size >= canvas.width || this.y - this.size <= 0){
+            this.directionY *= -1
+        }
+    }
+    draw(){
+        if(rainbow){
+            ctx.fillStyle= this.c;
+            ctx.strokeStyle= this.c;
+        }
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI*2);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+    }
+}
+
+function makeBall(x, y){
+    for(i=0; i<ballNumber; i++){
+        balls[i] = new Ball(x, y)
+    }
+}
+
+function ballAnimate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for(i=0; i<ballNumber; i++){
+        balls[i].update();
+        balls[i].draw();
+    }
+    if(balling){
+        requestAnimationFrame(ballAnimate);
+    }
+}
+
+function onMouseDown(event) {
+    if(rainbow){
+        color = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
+        ctx.strokeStyle = color;
+        ctx.fillStyle = color;
+    }
+
+    startPainting();
+
+    ballNumber++
+    if(balling){
+        clear.innerText = "STOP";
+        makeBall(event.offsetX, event.offsetY);
+        if(ballNumber === 1){
+            ballAnimate();
+        }
+    }
+}
+
+ 
 function onMouseMove(event) {
-    const x = event.offsetX;
-    const y = event.offsetY;
+    let x = event.offsetX
+    let y = event.offsetY
     if(!painting){
         ctx.beginPath();
         ctx.moveTo(x, y);
@@ -39,10 +116,17 @@ function onMouseMove(event) {
         ctx.lineTo(x, y);
         ctx.stroke();
     }
+
 }
 
 function handleColorClick(event) {
-    const color = event.target.style.backgroundColor;
+    if(!event.target.classList.contains("rainbow")){
+        color = event.target.style.backgroundColor;
+
+    } else {
+        rainbow = true;
+        color = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`; 
+    }
     ctx.strokeStyle = color;
     ctx.fillStyle = color;
 }
@@ -52,20 +136,34 @@ function handleRangeChange(event){
     ctx.lineWidth = size;
 }
 
+// function handleRadiusChange(event){
+//     const size = event.target.value;
+//     radiusSize = size;
+// }
+
 function handleModeClick(event) {
     if(filling === true){
+        balling = true;
         filling = false;
-        mode.innerText = "Fill"
+        mode.innerText = "Ball";
+    } else if (balling === true){
+        balling = false;
+        mode.innerText = "Paint";
     } else {
         filling = true;
-        mode.innerText = "Paint"
+        mode.innerText = "Fill";
     }
 }
 
-function handleClearClick(event) {
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = ctx.strokeStyle;
+function handleClearClick() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    if (balling){
+        balling = false;
+        clear.innerText = "Clear"
+        mode.innerText = "Paint";
+    } else {
+        ballNumber = 0;
+    }
 }
 
 function handleCanvasClick(){
@@ -84,10 +182,10 @@ function handleCM(event){
 
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mousedown", onMouseDown);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
-    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("mousedown", handleCanvasClick);
     canvas.addEventListener("contextmenu", handleCM)
 }
 
@@ -98,6 +196,10 @@ Array.from(colors).forEach(color =>
 if(range){
     range.addEventListener("input", handleRangeChange)
 };
+
+// if(radius){
+//     radius.addEventListener("input", handleRadiusChange)
+// };
 
 if(mode){
     mode.addEventListener("click", handleModeClick)
