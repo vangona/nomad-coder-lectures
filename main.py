238808f6c78ec62ca8f84a792c76690e4c59aa16
vangona <rlarhksrud12@naver.com>
2021-06-06 18:16:45 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, redirect, send_file
+from scrapper import get_jobs
+from exporter import save_to_file
+
+app = Flask("ScapperPractice")
+
+db = {}
+
+
+@app.route("/")
+def home():
+  return render_template("index.html")
+
+
+@app.route("/report")
+def report():
+  word = request.args.get("word")
+  if word:
+    word = word.lower()
+    existingJobs = []
+    if True:
+      jobs = existingJobs
+    else :
+      jobs = get_jobs(word)
+      db[word] = jobs
+  else:
+    redirect("/")
+  return render_template(
+    "report.html", 
+    searchingBy=word,
+    resultNumber=len(jobs),
+    jobs = jobs
+  )
+
+
+@app.route("/export")
+def export():
+  try:
+    word = request.args.get("word")
+    if not word:
+      raise Exception()
+    word = word.lower()
+    jobs = db.get(word)
+    if not jobs:
+      raise Exception()
+    save_to_file(jobs)
+    return send_file("jobs.csv")
+
+  except:
+    return redirect("/")
+
+
+
+app.run(host="0.0.0.0")
