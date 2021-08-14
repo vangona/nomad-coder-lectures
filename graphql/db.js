cfe04865213097dce3ca7752413dbe7e5bd7f4a2
vangona <rlarhksrud12@naver.com>
2021-08-14 16:02:53 +0900

    2021.08.14 lecture

--- graphql/db.js
+++ graphql/db.js
@@ -1,19 +1,39 @@
-export const people  = [
+let movies  = [
     {
-        id : "0",
-        name : "Gona",
-        age: 25,
-        gender: "male"
+        id : 0,
+        name : "Kimino Namaewa",
+        score: 4
     },
     {
-        id : "1",
-        name : "DDAT",
-        age: 21,
-        gender: "female"
+        id : 1,
+        name : "Avengers",
+        score: 3
     }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === String(id))
-    return filteredPeople[0]
+    const filteredMovies = movies.filter(movie => movie.id === (id))
+    return filteredMovies[0]
+}
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id != (id));
+    if(movies.length > cleanedMovies.length) {
+        movies = cleanedMovies
+        return true
+    } else {
+        return false
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: movies.length + 1,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
 }
