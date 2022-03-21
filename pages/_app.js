3f4c6956758529fe6ec127cdb822fdb67a45e235
vangona <rlarhksrud12@naver.com>
2022-03-21 17:32:01 +0900

    Add: 노마드코더 Next.js 강의 #1.7 Recap까지 (Next.js 개론)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import NavBar from "../components/NavBar";
+import "../styles/globals.css"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function CustomApp({ Component, pageProps }) {
+  return (
+    <div>
+      <NavBar />
+      <Component {...pageProps} />
+      <style jsx global>{`
+        a {
+          color: white;
+        }
+      `}</style>
+    </div>
+  )
 }
-
-export default MyApp
