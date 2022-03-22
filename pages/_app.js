6eae7e55e9e2301639286061b9489757a36cb7c6
vangona <rlarhksrud12@naver.com>
2022-03-22 15:03:00 +0900

    #2.0 Patterns

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import Layout from "../components/Layout";
+import NavBar from "../components/NavBar";
+import "../styles/globals.css"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function CustomApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
-
-export default MyApp
