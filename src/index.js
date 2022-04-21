d255fa1c5179bbf5bf5f134afd6f06db683ebf4d
vangona <rlarhksrud12@naver.com>
2022-04-21 16:18:39 +0900

    Add: #3.1 Connecting the Store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./components/App";
+import store from "./store";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
-root.render(<App></App>)
+root.render(
+  <Provider store={store}>
+    <App></App>
+  </Provider>
+);
